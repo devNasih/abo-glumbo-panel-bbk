fad1f0477df2eb7870495d55b763cf214aa49442
Han5uk3 <adnanyousufpangat@gmail.com>
2025-12-01 16:08:36 +0530

    feat: Removed unused imports.

--- lib/pages/home/worker/earnings.dart
+++ lib/pages/home/worker/earnings.dart
@@ -9,7 +9,6 @@ import 'package:aboglumbo_bbk_panel/pages/account/bloc/account_bloc.dart';
 import 'package:aboglumbo_bbk_panel/pages/home/worker/payout_requests.dart';
 import 'package:aboglumbo_bbk_panel/services/app_services.dart';
 import 'package:aboglumbo_bbk_panel/styles/color.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
-
 import 'package:aboglumbo_bbk_panel/helpers/firestore.dart';
 import 'package:aboglumbo_bbk_panel/helpers/local_store.dart';
 import 'package:aboglumbo_bbk_panel/pages/home/home.dart';
 import 'package:aboglumbo_bbk_panel/pages/login/signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
-
 import 'package:flutter/material.dart';
 
 class AuthServices {
